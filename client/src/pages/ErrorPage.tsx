03cb5689b99db8c7b4edc3443989ac2d2aeeb155
unknown <rosa.angel.daniel@gmail.com>
2024-12-27 11:23:45 -0700

    completed error page

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+    return (
+        <>
+         <h1> Error 404: Page not Found </h1>
+         <h1>¯\_(ツ)_/¯</h1>
+        </>
+    );
+};
+
+export default ErrorPage;
