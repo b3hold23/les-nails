f7ee6c3ce60180522b6336b8a6a55fcf072e5ab4
unknown <rosa.angel.daniel@gmail.com>
2025-01-16 14:50:01 -0700

    completed review api for front end

--- client/src/api/reviewAPI.tsx
+++ client/src/api/reviewAPI.tsx
@@ -1,1 +1,34 @@
 import { ReviewData } from "../interfaces/reviewData";
+
+const retrieveReviews = async () => {
+    try {
+        const response = await fetch('/api/reviews');
+        const data: ReviewData[] = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const createReview = async (body: ReviewData) => {
+    try {
+        const response = await fetch(
+            '/api/reviews/', {
+                method: 'POST',
+                body: JSON.stringify(body),
+            }
+        );
+        const data = await response.json();
+
+        if(!response.ok) {
+            throw new Error("Could not create review");
+        }
+
+        return data;
+    } catch (err) {
+        console.log('Error creating review:', err);
+        return Promise.reject(err);
+    }
+};
+
+export { retrieveReviews, createReview };
